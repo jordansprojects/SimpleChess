95a82ed1bcf323618668b4962a5fc29d75dc98f2
unknown <zach.jilani1@gmail.com>
2023-07-23 11:17:24 -0600

    added functions for generating moves

--- BoardRepresentation/pieces.h
+++ BoardRepresentation/pieces.h
@@ -1,8 +1,8 @@
 #ifndef PIECES_H
 #define PIECES_H
 
-
-enum Team {
+enum Team
+{
 	WHITE,
 	BLACK,
 	BLUE, /* support for chess beyond 2 teams!  */
@@ -15,54 +15,62 @@ enum Team {
 /****************************************************
  * Abstract class to represent objects on the board
  ****************************************************/
-class Piece{
-	public:
-		virtual void move() = 0;
-		virtual void generateMoves() = 0;
-	protected:
-		int square;
-		Team team;
+class Piece
+{
+public:
+	virtual void move() = 0;
+	virtual void generateMoves() = 0;
+
+protected:
+	int square;
+	Team team;
+
+	void generateRookMoves();
+	void generateBishopMoves();
+	void generateKnightMoves();
 }; // end of Piece class
 
 /****************************************************
  *
  ****************************************************/
-class Queen : Piece{
-};
-
-
-/****************************************************
- *
- ****************************************************/
-class Knight : Piece{
-};
-
-
-/****************************************************
- *
- ****************************************************/
-class Rook : Piece{
-
+class Queen : Piece
+{
 };
 
 /****************************************************
  *
  ****************************************************/
+class Knight : Piece
+{
+};
 
-class Bishop: Piece{
+/****************************************************
+ *
+ ****************************************************/
+class Rook : Piece
+{
+};
 
+/****************************************************
+ *
+ ****************************************************/
+
+class Bishop : Piece
+{
 };
 
 /****************************************************
  *
  * I understand a pawn is not technically "a piece" :-)
  ****************************************************/
-class Pawn: Piece{
+class Pawn : Piece
+{
 };
 
 /****************************************************
  *
  ****************************************************/
-class King: Piece{
+class King : Piece
+{
 };
 #endif
