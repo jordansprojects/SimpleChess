7a14d777cb77fe39448af808c2714c73b8cb01e9
jordansprojects <jordsvpn@protonmail.com>
2024-09-13 14:33:26 -0700

    added note. make gradle compile CMake and Google test deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,16 @@
  * This generated file contains a sample C++ project to get you started.
  * For more details on building C++ applications and libraries, please refer to https://docs.gradle.org/8.10/userguide/building_cpp_projects.html in the Gradle documentation.
  */
+// TO-DO: Configure to build CMake GTests from here
+
 plugins {
     // Apply the cpp-application plugin to add support for building C++ executables
     `cpp-application`
 
     // Apply the cpp-unit-test plugin to add support for building and running C++ test executables
     `cpp-unit-test`
+
+    id("io.github.isning.gradle.plugins.cmake") version "0.1.5"
 }
 
 // Set the target operating system and architecture for this application
